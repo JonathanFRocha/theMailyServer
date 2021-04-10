58ffd5da4f77a400ca5f52d5e1e9e09994ba438e
Jonathan Rocha <jonathanferreira.contato@gmail.com>
2021-04-10 19:20:02 -0300

    finished implementing cookie session

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { mongoURI } = require("./configs/keys");
-require("./services/passport");
+const cookieSession = require("cookie-session");
+const { mongoURI, cookieKey } = require("./configs/keys");
+const passport = require("passport");
 require("./models/User");
+require("./services/passport");
 
 const app = express();
 mongoose.connect(mongoURI, {
@@ -10,6 +12,17 @@ mongoose.connect(mongoURI, {
   useCreateIndex: true,
   useUnifiedTopology: true,
 });
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,17 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const { googleClientID, googleSecretKey } = require("../configs/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => done(null, user));
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +21,18 @@ passport.use(
       callbackURL: "/auth/google/callback",
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log(`access Token: ${accessToken}`);
-      console.log(`refresh Token: ${refreshToken}`);
-      console.log("teste ", profile);
+      User.findOne({ googleId: profile.id }).then((existingUser) => {
+        if (existingUser) {
+          console.log("abc");
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id,
+          })
+            .save()
+            .then((user) => done(null, user));
+        }
+      });
     }
   )
 );
